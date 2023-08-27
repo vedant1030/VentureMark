e81bdf37c38d0e156f26f754cebd355f3d6a5b66
Harkirat Soomal <106408296+Harkirat78@users.noreply.github.com>
2023-08-26 20:45:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,40 @@ document.addEventListener("DOMContentLoaded", function () {
     const resultMessage = document.querySelector(".result-message");
     const availableSpan = document.querySelector(".available");
 
-    form.addEventListener("submit", function (event) {
+    form.addEventListener("submit", async function (event) {
         event.preventDefault();
 
-        // Simulate availability check (you can replace this with your actual logic)
-        const isAvailable = Math.random() < 0.5;
+        const trademarkInput = form.querySelector("input[name='trademark']").value;
 
-        if (isAvailable) {
-            availableSpan.textContent = "available";
-            resultMessage.style.color = "#27ae60";
-        } else {
-            availableSpan.textContent = "not available";
-            resultMessage.style.color = "#e74c3c";
+        // Make an API request to your Flask backend
+        try {
+            const response = await fetch("/check-trademark", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ trademark: trademarkInput }),
+            });
+
+            if (!response.ok) {
+                throw new Error("Failed to fetch data");
+            }
+
+            const data = await response.json();
+
+            if (data.available) {
+                availableSpan.textContent = "available";
+                resultMessage.style.color = "#27ae60";
+            } else {
+                availableSpan.textContent = "not available";
+                resultMessage.style.color = "#e74c3c";
+            }
+
+            // Show the result message
+            resultMessage.style.display = "block";
+        } catch (error) {
+            console.error(error);
+            // Handle the error, e.g., display an error message to the user
         }
     });
 });
